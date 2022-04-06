ce12b8df7c0ad1ce9e480babf8ab5acaf7cf1f18
ArpitJ2912 <91619722+ArpitJ2912@users.noreply.github.com>
2022-04-06 18:21:05 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeleteComponent } from './delete/delete.component';
+import { DisplayComponent } from './display/display.component';
+import { RegisterComponent } from './register/register.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'display', component:DisplayComponent},
+  {path:'search', component:SearchComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'delete', component:DeleteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
